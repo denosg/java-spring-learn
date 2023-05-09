c1bad6e148416786ff60db6e317a095cbb32ba0f
denos <costelasdenis@gmail.com>
2023-05-09 16:45:51 +0300

    @Primary annotation

--- src/main/java/com/costelas/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/costelas/learnspringframework/App02HelloWorldSpring.java
@@ -2,7 +2,11 @@ package com.costelas.learnspringframework;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 public class App02HelloWorldSpring {
+
+    // Bean takes the names of the methods and saves them as pointers (I guess)
 
     public static void main(String[] args) {
         // 1: Launch a Spring Context -
@@ -16,6 +20,12 @@ public class App02HelloWorldSpring {
         System.out.println(context.getBean("name"));
         System.out.println(context.getBean("age"));
         System.out.println(context.getBean("person"));
-        System.out.println(context.getBean("address"));
+        System.out.println(context.getBean("address2"));
+//        System.out.println(context.getBean(Address.class));
+        System.out.println(context.getBean("person2MethodCall"));
+        System.out.println(context.getBean("person3Parameters"));
+        System.out.println(context.getBean("person4Parameters"));
+
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
     }
 }
--- src/main/java/com/costelas/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/costelas/learnspringframework/HelloWorldConfiguration.java
@@ -2,8 +2,9 @@ package com.costelas.learnspringframework;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
-record Person(String name, int age) { }
+record Person(String name, int age, Address address) { }
 
 record Address(String firstLine, String city) { }
 
@@ -22,11 +23,32 @@ public class HelloWorldConfiguration {
 
     @Bean
     public Person person() {
-        return new Person("denis", 21);
+        return new Person("denis", 21, new Address("NU", "Zalau"));
     }
 
     @Bean
+    public Person person2MethodCall() {
+        return new Person(name(), age(), address()); // name, age
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age, Address address3) { //name, age, address3
+        return new Person(name, age, address3); // name, age
+    }
+
+    @Bean
+    public Person person4Parameters(String name, int age, Address address) { //name, age, address
+        return new Person(name, age, address); // name, age
+    }
+
+    @Bean(name = "address2")
+    @Primary
     public Address address() {
         return new Address("Str.Ioan Nichita", "Zalau");
+    }
+
+    @Bean(name = "address3")
+    public Address address3() {
+        return new Address("Ce plm", "Salaj");
     }
 }
